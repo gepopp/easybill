6cfa01c9182229dcefcc68d7876178f709cc1a46
Gerhard Popp <gerhrad@poppgerhard.at>
2020-10-25 00:08:16 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    dd('Hello World'); return view('welcome');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
