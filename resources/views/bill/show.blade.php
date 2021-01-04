f2f21c3cfd0962a78c408491dec03392c4a62c93
Gerhard Popp <gerhrad@poppgerhard.at>
2021-01-04 18:18:48 +0100

    update customer create

--- resources/views/bill/show.blade.php
+++ resources/views/bill/show.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 px-20">
                 @if($bill->sent_at == null)
-                    <a href="{{ route( 'bill.send', $bill ) }}" class="p-3 px-5 text-center text-white font-semibold shadow-lg bg-red-800 inline-block" target="_blank">
+                    <a href="{{ route( 'bill.send', $bill ) }}" class="p-3 px-5 text-center text-white font-semibold shadow-lg bg-red-800 inline-block">
                     <span class="flex space-x-4">
                        <svg class="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"></path></svg>
                         <p>Rechnung jetzt senden</p>
