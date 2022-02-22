8e1503538388df95f6bf2496fedc68b36067b1f0
Gerhard Popp <gerhrad@poppgerhard.at>
2022-02-22 17:43:36 +0100

    mail marketing

--- resources/views/newsletter/raw.blade.php
+++ resources/views/newsletter/raw.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>Corp</title>
+		<title>Webentwicklung Gerhard Popp</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<meta  name="viewport" content="width=display-width, initial-scale=1.0, maximum-scale=1.0," />
 		<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" type='text/css' />
